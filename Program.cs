3782b470531fa3cd0ecfc7961e9b8c27f695840d
David05 <davidgreendevlops@gmail.com>
2026-02-21 14:02:51 +0100

    first write-uo|

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NewCSharp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      float x = 10.97f, y = 20.8484f, z = 30f;
+      int ww =  (int)x;
+      double dd = (double)y;
+      Console.WriteLine($"x: {ww}, y: {dd}, z: {z}");
+      // Console.WriteLine($"Name: {name}, Length: {length}");
+    }
+  }
+}
